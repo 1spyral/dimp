0dcef4ce2bedd679166665600b02ee21ff8ce651
Luke Zhan <luke.zhan@uwaterloo.ca>
2025-12-27 17:51:11 -0500

    [clyde-dashboard] Tweak eslint config and plugins
    Fix `target="_blank"` vulnerability raised by linter

--- clyde-dashboard/eslint.config.js
+++ clyde-dashboard/eslint.config.js
@@ -1,23 +1,38 @@
 import js from "@eslint/js"
 import globals from "globals"
+import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
 import tseslint from "typescript-eslint"
+import prettier from "eslint-config-prettier/flat"
 import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
     globalIgnores(["dist"]),
     {
         files: ["**/*.{ts,tsx}"],
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+        },
         extends: [
             js.configs.recommended,
             tseslint.configs.recommended,
+            react.configs.flat.recommended,
             reactHooks.configs.flat.recommended,
-            reactRefresh.configs.vite,
         ],
         languageOptions: {
             ecmaVersion: 2020,
+            sourceType: "module",
             globals: globals.browser,
         },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
     },
+    prettier,
 ])
