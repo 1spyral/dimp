d951dda278c62c8131b538f0430f520645f64691
Luke Zhan <luke.zhan@uwaterloo.ca>
2026-01-25 14:49:37 -0500

    Create public JWKS endpoint (#136)

--- dimp-auth/src/server.ts
+++ dimp-auth/src/server.ts
@@ -1,10 +1,24 @@
 import Fastify from "fastify"
 import { loggerConfig } from "@/logger"
+import { JwksStore } from "@/jwks"
 
 const fastify = Fastify({ logger: loggerConfig })
+const jwksStore = new JwksStore()
 
 fastify.get("/", async (_request, reply) => {
     return reply.code(200).type("text/plain").send("Healthcheck passed")
+})
+
+fastify.get("/.well-known/jwks.json", async (_request, reply) => {
+    return reply.code(200).send(jwksStore.getPublicJwks())
+})
+
+fastify.addHook("onReady", async () => {
+    await jwksStore.start()
+})
+
+fastify.addHook("onClose", async () => {
+    jwksStore.stop()
 })
 
 export { fastify as server }
